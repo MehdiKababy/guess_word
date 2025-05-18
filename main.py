284c91fc86bb710981a95355e2c5de437926aef5
MehdiKababy <mehdikababy@gmail.com>
2025-05-18 09:48:34 +0330

    update main.py

--- main.py
+++ main.py
@@ -12,21 +12,22 @@ with open("guess_word/Include/words.json", "r") as words_file:
         word = random.choice(word_selected)
         word_selected = word_selected.replace(word, "_")
         Run -= 1
+    false_characters = []
     while True:
         print(word_selected)
-        print(word_selected_copy)
         if word_selected == word_selected_copy:
+            print("You win")
+            print(f'Wrong words : {false_characters}')
             break
-        input_character = input("Enter your character : ")
-        # false_characters = []
+        input_character = input("Enter the character : ")
+        input_character = input_character.lower()
         if len(input_character) < 2:
+            word_selected_list = list(word_selected)
             if input_character in word_selected_copy:
                 if word_selected_copy.count(input_character) == 1:
                     index = word_selected_copy.index(input_character)
-                    word_selected_list = list(word_selected)
                     word_selected_list[index] = input_character
                 else:
-                    word_selected_list = list(word_selected)
                     for i, i_2 in enumerate(word_selected_copy):
                         if i_2 == input_character:
                             word_selected_list[i] = i_2
@@ -34,7 +35,9 @@ with open("guess_word/Include/words.json", "r") as words_file:
                 for i in word_selected_list:
                     result += i
                 word_selected = result
+                print(f'This {input_character} word is correct\n')
             else:
-                print("false")
+                print("This character does not exist in the word\n")
+                false_characters.append(input_character)
         else:
-            print("Enter one character\n")
+            print("Enter only one character\n")
